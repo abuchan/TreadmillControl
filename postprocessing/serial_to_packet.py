42a54bda5312c9791cfa3c7c93bec6aba08ac425
abuchan <austin.d.buchan@gmail.com>
2013-03-02 17:07:12 -0800

    Pulls datafiles from sbRIO over FTP

--- postprocessing/serial_to_packet.py
+++ postprocessing/serial_to_packet.py
@@ -1,5 +1,56 @@
-from struct import *
+from struct import unpack
+from ftplib import FTP
+from glob import glob
+from os import makedirs
 
+# Remote sbRIO address and folder
+ftp_addr = '10.0.0.2'
+ftp_dir = 'logs'
+
+# Where to find OptitrackLogs
+optitrack_dir = ''
+
+# Where collected files get put
+logs_dir = './logs/'
+
+# Get the most recent num files, all if num is -1
+def fetch_and_parse(num):
+   host = FTP(ftp_addr)
+   host.login()
+   host.cwd(ftp_dir)
+   dirlist = []
+   host.dir(dirlist.append)
+   
+   if num is -1:
+      file_idx = range(0,len(dirlist),2)
+   else:
+      file_idx = range(-2*num,0,2)
+   
+   log_count = 0
+   for i in file_idx:
+      ctrl_filename = dirlist[i].split()[-1]
+      datestring = ctrl_filename[0:19]
+      local_dir = logs_dir + datestring + '/'
+      
+      if len(glob(local_dir)) is not 0:
+         print 'Error: %s directory already exists' % datestring
+      else:
+         makedirs(local_dir)
+         local_ctrl_file = open(local_dir + ctrl_filename,'wb')
+         host.retrbinary('RETR ' + ctrl_filename, local_ctrl_file.write)
+         local_ctrl_file.close()
+         
+         robot_filename = dirlist[i+1].split()[-1]
+         local_robot_file = open(local_dir + robot_filename, 'wb')
+         host.retrbinary('RETR ' + robot_filename, local_robot_file.write)
+         local_robot_file.close()
+         
+         parse_datafile(local_dir + robot_filename)
+         
+         log_count = log_count + 1
+      
+   print '%d logs read' % log_count
+         
 def extract_packet(data,index):
    size = ord(data[index])
    size_check = ord(data[index+1])
